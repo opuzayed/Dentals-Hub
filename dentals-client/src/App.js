d99dc3f7a941e7470225102c52e113749e346f4f
opuzayed <zayedopu4933@gmail.com>
2023-01-15 20:43:32 +0600

    Basic project setup with pages, routes and layout done

--- dentals-client/src/App.js
+++ dentals-client/src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes/Routes";
 
 
 function App() {
   return (
     <div>
-      
+      <RouterProvider router={router} />
     </div>
   );
 }
