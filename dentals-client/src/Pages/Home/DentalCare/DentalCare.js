6d082c254fdf905926d2d60a18fe4ee19f3cc941
opuzayed <zayedopu4933@gmail.com>
2023-01-18 15:29:46 +0600

    dentalcare with responsive

---
+++ dentals-client/src/Pages/Home/DentalCare/DentalCare.js
@@ -0,0 +1,20 @@
+import React from "react";
+import treatment from '../../../assets/images/treatment.png';
+const DentalCare = () => {
+  return (
+    <div className="hero mt-5">
+  <div className="hero-content flex-col lg:flex-row">
+    <div className='md:w-1/2'>
+    <img src={treatment} className="rounded-lg shadow-2xl w-[322px] h-[406px]  md:mx-56" alt="#"/>
+    </div>
+    <div className='md:w-1/2'>
+      <h1 className="text-5xl font-bold ">Exceptional Dental <br /> Care, on Your Terms</h1>
+      <p className="py-6">Learn how to keep your mouth healthy and happy all day long. Don't let poor oral health ruin your beautiful smile. </p>
+      <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
+    </div>
+  </div>
+</div>
+  );
+};
+
+export default DentalCare;
