dd22521c730f382b655cf1118cefa43997df1c45
opuzayed <zayedopu4933@gmail.com>
2023-01-22 23:53:46 +0600

    login page validation

--- dentals-client/src/Pages/Login/Login.js
+++ dentals-client/src/Pages/Login/Login.js
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const {register, handleSubmit} = useForm();
+    const {register, formState: { errors }, handleSubmit} = useForm();
 
     const handleLogin = data => {
         console.log(data);
@@ -11,15 +11,24 @@ const Login = () => {
     return (
         <div className='h-[800px] shadow-2xl flex justify-center items-center'>
             <div className='w-96 p-7 shadow-2xl'>
-                <h2 className='text-4xl mb-5 text-center'>Please Login</h2>
+                <h2 className='text-4xl mb-5 text-center font-medium'>Please Login</h2>
                 <form onSubmit={handleSubmit(handleLogin)}>
                 <div className="form-control w-full">
                     <label className="label"><span className="label-text">Email</span></label>
-                    <input type="email" {...register("email")} className="input input-bordered w-full max-w-xs" />
+                    <input type="email" {...register("email", { required: "Email Address is required" })} className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-error text-center'>{errors.email?.message}</p>}
                 </div>
                 <div className="form-control w-full">
                     <label className="label"><span className="label-text">Password</span></label>
-                    <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+                    <input type="password" {...register("password",
+                    { required: "Password is required",
+                    minLength:{ value: 6, message: "Password must be at least six characters" },
+                    maxLength:{ value: 10, message: "Password maximum ten characters" } 
+                }
+                    
+                    // { maxLength: { value: 10, message: "Password maximum ten characters" } }
+                    )} className="input input-bordered w-full max-w-xs" />
+                    {errors.password && <p className='text-error text-center'>{errors.password?.message}</p>}
                 </div>
                 <input className='btn btn-accent w-full mt-5' type="submit" value="Login" />
                 </form>
