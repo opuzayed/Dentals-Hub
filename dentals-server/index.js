8108e987014c0a0c91e3bda3f9dec23cb10bc073
opuzayed <zayedopu4933@gmail.com>
2023-01-27 21:23:19 +0600

    hide user name and password by .env

--- dentals-server/index.js
+++ dentals-server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
 const app = express();
 
 //middleware
@@ -10,13 +12,9 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 
 
 app.get('/', async (req, res) => {
