b1ceb17a44959a770f04a0658f3343d3a8510c02
opuzayed <zayedopu4933@gmail.com>
2023-01-27 16:23:45 +0600

    is ok server side

--- dentals-server/index.js
+++ dentals-server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', async (req, res) => {
     res.send('Dental point server is running');
