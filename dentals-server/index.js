fdacce4a62adb99377161acb77385e4d9e63d068
opuzayed <zayedopu4933@gmail.com>
2023-01-28 02:02:47 +0600

    load appointmentOptions from db

--- dentals-server/index.js
+++ dentals-server/index.js
@@ -11,10 +11,25 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+  try{
+      const appointOptionCollection = client.db("dentalPoint").collection("appointmentOptions");
+    
+      app.get('/appointmentOptions', async(req, res) => {
+        const query = {};
+        const options = await appointOptionCollection.find(query).toArray();
+        res.send(options);
+      });
+
+  }
+  finally{
+
+  }
+}
+run().catch(console.log());
 
 
 app.get('/', async (req, res) => {
