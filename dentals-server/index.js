0a4850be8aea2d9e37d75d5acfa72e662b3e67d6
opuzayed <zayedopu4933@gmail.com>
2023-01-29 03:47:11 +0600

    partially done available appointments

--- dentals-server/index.js
+++ dentals-server/index.js
@@ -21,8 +21,16 @@ async function run() {
       const bookingsCollection = client.db('dentalPoint').collection('bookings');
 
       app.get('/appointmentOptions', async(req, res) => {
+        const date = req.query.date;
         const query = {};
         const options = await appointOptionCollection.find(query).toArray();
+        const bookingQuery = {appointmentDate : date};
+        const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+        options.forEach(option => {
+          const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+          const bookedSlots = optionBooked.map(book => book.slot);
+          console.log(date, option.name, bookedSlots);
+        })
         res.send(options);
       });
 
