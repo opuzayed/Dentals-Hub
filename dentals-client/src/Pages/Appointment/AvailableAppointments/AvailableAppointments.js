b650c9ac4427eb55bf95a452cf55c487a72de573
opuzayed <zayedopu4933@gmail.com>
2023-01-28 22:47:37 +0600

    data load by react query function

--- dentals-client/src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ dentals-client/src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,18 +1,22 @@
 import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
 import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
+
 const AvailableAppointments = ({dateSelected}) => {
    
-    const [appointmentOptions, setAppointmentOptions] = useState([]);
     const [treatment, setTreatment] = useState(null);
-    
-    useEffect(()=>{
-        fetch('http://localhost:5000/appointmentOptions')
-        .then(res => res.json())
-        .then(data =>setAppointmentOptions(data))
-    },[]);
+
+    const {data:appointmentOptions=[]} = useQuery({
+        queryKey:'appointmentOptions',
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/appointmentOptions')
+            const data = await res.json();
+            return data;
+          },
+        });
     
     return (
         <section className='mt-32'>
