f8b39a68d75a2d293304aedac1c53e778510a302
opuzayed <zayedopu4933@gmail.com>
2023-02-01 01:13:36 +0600

    basic jwt structure done with jwt function

--- dentals-server/index.js
+++ dentals-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -61,14 +62,27 @@ async function run() {
         res.send(result);
       });
 
+      app.get('/jwt', async(req, res) => {
+        const email = req.query.email;
+        const query = {email : email};
+        const user = await usersCollection.findOne(query);
+        if(user)
+        {
+          const token = jwt.sign({email}, process.env.JWT_SECRET, { expiresIn: '1h' });
+          return res.send({accessToken : token});
+        }
+        res.status(403).send({accessToken : ''});
+      });
+
       app.post('/users', async (req,res) => {
         const user = req.body;
         const result = await usersCollection.insertOne(user);
         res.send(result);
       })
   }
-  finally{
-
+  finally
+  {
+      
   }
 }
 run().catch(console.log);
