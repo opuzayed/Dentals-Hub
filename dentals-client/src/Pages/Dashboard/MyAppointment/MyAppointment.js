7a1e141968ad8a4af5a6e195e5ccc1db9e484748
opuzayed <zayedopu4933@gmail.com>
2023-01-31 22:12:23 +0600

    dashboard responsive done and color problem fixed

--- dentals-client/src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ dentals-client/src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -17,23 +17,25 @@ const MyAppointment = () => {
   });
   console.log("bookings", bookings);
   return (
-    <div className="bg-stone-100 rounded-md">
+    <div className="rounded-md">
       <div className="p-12">
         <h3 className="text-3xl pb-5">My Appointment</h3>
         <div className="overflow-x-auto">
           <table className="table w-full">
             <thead>
-              <tr>
+              <tr className="dark:text-black">
                 <th></th>
                 <th>Name</th>
                 <th>Treatment</th>
-                <th>Apponitment Date</th>
+                <th>Appointment Date</th>
                 <th>Time</th>
               </tr>
             </thead>
             <tbody>
               {bookings.map((booking, i) => (
-                <tr className="hover">
+                <tr className="hover dark:text-black"
+                key={i}
+                >
                   <th>{i + 1}</th>
                   <td>{booking.patientName}</td>
                   <td>{booking.treatment}</td>
