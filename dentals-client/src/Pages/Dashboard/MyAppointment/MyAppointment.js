e43c995689b3acc7ed5fc7a0eeb07deb3e2f3bed
opuzayed <zayedopu4933@gmail.com>
2023-02-01 23:45:53 +0600

    custom hook impliment

--- dentals-client/src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ dentals-client/src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -9,7 +9,11 @@ const MyAppointment = () => {
     queryKey: ["bookings", user?.email],
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/bookings?email=${user?.email}`
+        `http://localhost:5000/bookings?email=${user?.email}`,{
+          headers : {
+            authorization : `bearer ${localStorage.getItem('accessToken')}`
+          }
+        }
       );
       const data = await res.json();
       return data;
