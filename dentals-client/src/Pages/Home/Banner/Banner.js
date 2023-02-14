729dff0f539deaec5cdfdede9b73766bca76047d
opuzayed <zayedopu4933@gmail.com>
2023-02-15 01:45:47 +0600

    fixed banner page button

--- dentals-client/src/Pages/Home/Banner/Banner.js
+++ dentals-client/src/Pages/Home/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import chair from '../../../assets/images/chair.png';
 import bgchair from '../../../assets/images/bg.png';
 import PrimaryButton from '../../../Components/PrimaryButton/PrimaryButton';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -11,7 +12,7 @@ const Banner = () => {
           <div>
             <h1 className="text-5xl font-bold">YOUR NEW SMILE STARTS <br /> Here</h1>
             <p className="py-6">Dental implants are artificial tooth root similar to a metal screw placed into the jawbone to hold artificial tooth or teeth. They are a treatment option for individuals who have lost tooth or teeth due to decay, gum disease or injury.</p>
-            <PrimaryButton>Get Started</PrimaryButton>
+            <Link to='/appointment'><PrimaryButton>Get Started</PrimaryButton></Link>
           </div>
         </div>
       </div>
