1dec14acd776cbeac1dd966a1d79bf9eb4105465
opuzayed <zayedopu4933@gmail.com>
2023-02-15 02:22:06 +0600

    post contact form to the server

--- dentals-client/src/Pages/Home/ContactUs/ContactUs.js
+++ dentals-client/src/Pages/Home/ContactUs/ContactUs.js
@@ -1,12 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
+import { useContext } from "react";
 import { useForm } from 'react-hook-form';
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 import appointment from '../../../assets/images/appointment.png';
 import PrimaryButton from "../../../Components/PrimaryButton/PrimaryButton";
+import { AuthContext } from "../../../contexts/AuthProvider";
 const ContactUs = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const [contactFormError, setContactFormError] = useState('');
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    //const [contactFormError, setContactFormError] = useState('');
+    const {user} = useContext(AuthContext);
 
-    const handleContactForm = (data) => console.log(data);
+    const navigate = useNavigate();
+
+    const handleContactForm = (data) => {
+      console.log(data);
+      if(data.email !== user?.email)
+      {
+        navigate('/login');
+      }
+      
+      else{
+        const contactForm = {
+          userEmail : data.email,
+          userMessage : data.message,
+          userSubject : data.subject
+    }
+    fetch("http://localhost:5000/contact", {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(contactForm),
+            })
+              .then((res) => res.json())
+              .then((result) => {
+                console.log(result);
+                toast.success("Message send successfully");
+                reset();
+              });
+      }
+
+    };
 
   return (
     <div
@@ -24,23 +59,23 @@ const ContactUs = () => {
                         <div className="form-control w-full max-w-xs mb-5">
                             <input type="email" {...register("email", {
                                 required: "Email is required"
-                            })} className="input input-bordered w-full max-w-xs text-black" placeholder="Your Email" />
-                            {errors.email && <p className='text-error'>{errors.email.message}</p>}
+                            })} className="input input-bordered w-full max-w-xs text-black" defaultValue ={user?.email} placeholder="Your Email" />
+                            {errors.email && <p className='text-error text-center'>{errors.email.message}</p>}
                         </div>
                     <div className="form-control w-full max-w-xs mb-5">
                         <input type="text" {...register("subject", {
                             required: "Subject is Required"
                         })} className="input input-bordered w-full max-w-xs text-black" placeholder="Your Subject" />
-                        {errors.subject && <p className='text-error'>{errors.subject.message}</p>}
+                        {errors.subject && <p className='text-error text-center'>{errors.subject.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                     <textarea {...register("message", {
                             required: "Message is Required"
                         })} className="textarea textarea-bordered text-black" placeholder="Your Message"></textarea>
-                    {errors.message && <p className='text-error'>{errors.message.message}</p>}
+                    {errors.message && <p className='text-error text-center'>{errors.message.message}</p>}
                     </div>
                     <div className="mt-9 w-32 mx-auto"><PrimaryButton size='lg'>Submit</PrimaryButton></div>
-                    {contactFormError && <p className='text-error'>{contactFormError}</p>}
+                    {/* {contactFormError && <p className='text-error'>{contactFormError}</p>} */}
                 </form>
         </div>
       </div>
